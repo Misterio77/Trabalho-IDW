5f6123418ef724f370653ace53a2dd3b02f05f43
Misterio77 <gsfontes2000@gmail.com>
2019-09-30 19:22:01 -0300

    Fixed links

--- assets/init.js
+++ assets/init.js
@@ -42,11 +42,5 @@
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
 
-$("input[name='phone']:first").keyup(function(e){
-	var key=String.fromCharCode(e.keyCode);
-	var value=$(this).val();
-	if(value.length==2)$(this).val($(this).val()+' ');
-	if(value.length==4|value.length==5)$(this).val($(this).val()+'-')
-});
 
 
